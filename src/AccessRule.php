7270213a4e1f65d800bbe0933204d68781f9c711
vladayson <vladaysin@gmail.com>
2019-03-25 13:16:12 +0300

    fixes

--- src/AccessRule.php
+++ src/AccessRule.php
@@ -2,42 +2,74 @@
 
 namespace vladayson\AccessRules;
 
+use vladayson\AccessRules\models\Roles;
 use yii\base\Component;
-use yii\helpers\ArrayHelper;
 
 /**
- * Class AccessRules.
+ * Class AccessRule.
  *
  * @package vladayson\AccessRules
  */
-//class AccessRule extends Component
-class AccessRule
+class AccessRule extends Component
 {
-    public $db = 'db';
-
     /**
-     * @param        $name
-     * @param string $description
-     *
-     * @return Roles
+     * @var array
      */
-    public function addRole($name, $description = '')
-    {
-        return (new Roles([
-            'name' => $name,
-            'description' => $description
-        ]))->createOrReturn();
-    }
+    public $roles = [];
 
     /**
-     * @param Roles $role
-     * @param Roles $childRole
+     * @var array
+     */
+    public $actions = [];
+
+    /**
+     * @var bool
+     */
+    public $allow = true;
+
+    /**
+     * @param $action
      *
      * @return bool
      */
-    public function addChild(Roles $role, Roles $childRole)
+    public function checkAccess($action)
     {
-        $role->roles = ArrayHelper::merge($role->roles, [$childRole]);
-        return $role->save();
+        $hasUser = false;
+        $userId = !\Yii::$app->user->isGuest ? \Yii::$app->user->id : 0;
+
+        foreach ($this->roles as $roleName) {
+            $usersIds = Roles::getUsersByRole($roleName);
+            if (in_array($userId, $usersIds)) {
+                $hasUser = true;
+                break;
+            }
+        }
+        if (!$hasUser) {
+            return false;
+        }
+        if (
+            empty($this->actions) ||
+            (
+                in_array('@', $this->roles) &&
+                $userId > 0
+            )
+        ) {
+            return $this->allow;
+        }
+        if (
+            in_array($action->id, $this->actions) &&
+            $hasUser
+        ) {
+            return $this->allow;
+        }
+        if (
+            !in_array($action->id, $this->actions) &&
+            in_array('*', $this->actions) &&
+            $hasUser
+        ) {
+            return $this->allow;
+        }
+
+        return false;
     }
 }
